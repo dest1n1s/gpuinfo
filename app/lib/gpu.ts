005ad680a5b197abaeb8d9f592a18011035712f4
Dest1n1 <dest1nis2022@gmail.com>
2024-11-15 17:03:37 +0800

    feat: use aggregation to list partitions

--- app/lib/gpu.ts
+++ app/lib/gpu.ts
@@ -1,5 +1,5 @@
 import { logger } from "@/logging.server";
-import { GPUInfo, NodeInfo, PartitionInfo } from "@/types/gpu";
+import { NodeInfo, PartitionInfo } from "@/types/gpu";
 import { db, mongoConnectPromise } from "./database/database";
 
 export const listPartitions = async (): Promise<PartitionInfo<string>[]> => {
@@ -10,42 +10,19 @@ export const listPartitions = async (): Promise<PartitionInfo<string>[]> => {
 export const listPartitionsDetailed = async (): Promise<PartitionInfo<NodeInfo>[]> => {
   logger.info("[listPartitionsDetailed] MongoDB request started");
   await mongoConnectPromise;
-  const response = await Promise.all(
-    (await listPartitions()).map(async partition => {
-      const nodes = await Promise.all(
-        partition.nodes.map(async node => {
-          const gpus = await Promise.all(
-            (await db.collection<GPUInfo>(node).find().toArray())
-              .reduce<GPUInfo[]>((acc, gpu) => {
-                const exist = acc.find(g => g.index === gpu.index);
-                if (exist) {
-                  if (gpu.time > exist.time) {
-                    return [...acc.filter(g => g.index !== gpu.index), gpu];
-                  }
-                  return acc;
-                }
-                return [...acc, gpu];
-              }, [])
-              .toSorted((a, b) => a.index - b.index)
-              .map(async gpu => {
-                const userInfo = await db
-                  .collection<{ user: string; start_time: Date }>(node + "-user")
-                  .findOne({ idx: gpu.index });
-                return {
-                  ...gpu,
-                  user: userInfo?.user ?? null,
-                  start_time: userInfo?.start_time ?? null,
-                };
-              }),
-          );
-
-          return { ip: node, gpus };
-        }),
-      );
-
-      return { ...partition, nodes };
-    }),
-  );
+  const response = await db
+    .collection("partitions")
+    .aggregate<PartitionInfo<NodeInfo>>([
+      {
+        $lookup: {
+          from: "latestNodes",
+          localField: "nodes",
+          foreignField: "ip",
+          as: "nodes",
+        },
+      },
+    ])
+    .toArray();
   logger.info("[listPartitionsDetailed] MongoDB request finished");
   return response;
 };
