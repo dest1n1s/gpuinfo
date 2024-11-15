ba2bb88e381949f735e50a49e7dab7cc1580bee0
Dest1n1 <dest1nis2022@gmail.com>
2024-11-15 17:09:34 +0800

    feat: add caching

--- app/lib/gpu.ts
+++ app/lib/gpu.ts
@@ -1,15 +1,34 @@
 import { logger } from "@/logging.server";
 import { NodeInfo, PartitionInfo } from "@/types/gpu";
+import { cache } from "./cache.server";
 import { db, mongoConnectPromise } from "./database/database";
 
 export const listPartitions = async (): Promise<PartitionInfo<string>[]> => {
   await mongoConnectPromise;
-  return await db.collection<PartitionInfo<string>>("partitions").find().toArray();
+
+  const cacheKey = "listPartitions";
+  const inCache = cache.get<PartitionInfo<string>[]>(cacheKey);
+  if (inCache) {
+    logger.info("[listPartitions] Cache hit");
+    return inCache;
+  }
+  logger.info("[listPartitions] MongoDB request started");
+  const response = await db.collection<PartitionInfo<string>>("partitions").find().toArray();
+  cache.set(cacheKey, response);
+  logger.info("[listPartitions] MongoDB request finished");
+  return response;
 };
 
 export const listPartitionsDetailed = async (): Promise<PartitionInfo<NodeInfo>[]> => {
-  logger.info("[listPartitionsDetailed] MongoDB request started");
   await mongoConnectPromise;
+
+  const cacheKey = "listPartitionsDetailed";
+  const inCache = cache.get<PartitionInfo<NodeInfo>[]>(cacheKey);
+  if (inCache) {
+    logger.info("[listPartitionsDetailed] Cache hit");
+    return inCache;
+  }
+  logger.info("[listPartitionsDetailed] MongoDB request started");
   const response = await db
     .collection("partitions")
     .aggregate<PartitionInfo<NodeInfo>>([
@@ -23,6 +42,8 @@ export const listPartitionsDetailed = async (): Promise<PartitionInfo<NodeInfo>[
       },
     ])
     .toArray();
+
+  cache.set(cacheKey, response);
   logger.info("[listPartitionsDetailed] MongoDB request finished");
   return response;
 };
